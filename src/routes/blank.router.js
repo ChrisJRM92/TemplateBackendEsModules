1cd3f953f25c9d057190de93c3fed549a961c693
Christian R <cjromerom28@gmail.com>
2025-05-22 21:37:10 -0500

    Update dependencies
    Update dependencies

--- src/routes/blank.router.js
+++ src/routes/blank.router.js
@@ -1,8 +1,1 @@
-import { getAll } from '../controllers/blank.controller.js';
-import express from 'express';
-
-const routeName = express.Router();
-
-routeName.route('/').get(getAll)
-
-export default routeName
+//use snippets
