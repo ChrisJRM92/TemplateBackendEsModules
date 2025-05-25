e225320e98883584393f4b5744c36869db1343ea
Christian R <cjromerom28@gmail.com>
2025-05-25 17:18:11 -0500

    Update template
    update template

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const router = express.Router()
+//import nameRouter from './name.router.js';
 
 //Place routes here
+// router.use('/urlpath', nameRouter)
 
 export default router;
